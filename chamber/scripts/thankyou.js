e9c19103865323d277808db554c4e43b34ab07e5
Eli LeBlanc <E1leblanc@outlook.com>
2026-06-09 15:06:23 -0500

    discover page

--- chamber/scripts/thankyou.js
+++ chamber/scripts/thankyou.js
@@ -1,9 +1,10 @@
-function displayObj(obj, parentElement) {
-    for (var key in obj) {
-        var p = document.createElement("p");
-        p.innerHTML = obj[key];
-        parentElement.appendChild(p);
-    }
-}
+const params = new URLSearchParams(window.location.search);
 
-displayObj(dataObj, formSection);
+const firstname = params.get('firstName');
+const lastname = params.get('lastName');
+const orgtitle = params.get('orgTitle');
+const email = params.get('email');
+const mobile = params.get('mobile');
+const orgname = params.get('orgName');
+const membership = params.get('membershipLevel');
+
